8e0056a9a165cb79769b256291d07278eed1e297
Tom Riley <tom.riley@outlook.com>
2022-04-02 14:23:40 +0100

    Initial Commit

---
+++ Random Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random Photo
+//
+//  Created by Tom Riley on 02/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
